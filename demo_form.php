de106f85a93905bcfcac028816e2267d67fa0f56
Dan Linford <dlinford@vt.edu>
2014-06-01 18:07:15 -0400

    This is a basic form to test out.

--- demo_form.php
+++ demo_form.php
@@ -1,6 +1,8 @@
 <html>
 <body>
 
+This is a basic form to test out.
+
 <form action="welcome.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
